963cd77982c575e9b7faac527a334cc79b915b53
Rob Donnelly <rfdonnelly@gmail.com>
2015-11-20 22:53:48 -0800

    add full support for user script methods
    Allow users to define methods using common Ruby syntax and allow them to
    be used inside of blocks in the job block.  See tests/blocks/def.avj for
    example.
    
    Previously only lambda method was supported.  See
    tests/blocks/lambda.avj for example.

--- lib/av/jobs/dsl/jobcommand.rb
+++ lib/av/jobs/dsl/jobcommand.rb
@@ -20,6 +20,33 @@ module AV
         def repeat(iterations)
           @iterations = iterations
         end
+
+        # enable use of methods defined in script scope
+        #
+        # For example, this enables use of 'tests' method in the following
+        # example user script.  'tests' is a method of the user script class.
+        # Code in 'execute' block only has access to JobCommand methods.  Any
+        # unrecognized JobCommand method (i.e. call to 'tests' method) will be
+        # passed to method_missing and then passed up to the user script.
+        #
+        # def tests
+        #   %w(test0 test1)
+        # end
+        #
+        # job :test do
+        #   execute do
+        #     "echo #{tests.sample}"
+        #   end
+        # end
+        def method_missing(method_sym, *arguments, &block)
+          script_obj = AV::Jobs::DSL::Loader.script
+          script_obj.send(method_sym, *arguments, &block)
+        end
+
+        def respond_to_missing?(method_sym, include_private = false)
+          script_obj = AV::Jobs::DSL::Loader.script
+          script_obj.respond_to?(method_sym, include_private)
+        end
       end
     end
   end
--- lib/av/jobs/dsl/loader.rb
+++ lib/av/jobs/dsl/loader.rb
@@ -5,6 +5,8 @@ module AV
         @@imports = []
         @@import = {}
         @@prefixes = []
+        @@script_objs = []
+        @@script_obj = nil
 
         def self.evaluate(prefix, valid_jobs, filename)
           filename = AV::Util.expand_envars(filename)
@@ -12,11 +14,13 @@ module AV
           @@imports.push(@@import) if @@import
           @@prefixes.push(prefix) if prefix
           @@import = {filename: filename, prefix: prefix, valid_jobs: valid_jobs}
-          script_obj = AV::Jobs::Script.new.from_file(filename, AV::Jobs::DSL::Commands)
-          @@import = @@imports.pop if @@imports.length > 0
+          @@script_objs.push(@@script_obj) if @@script_obj
+          @@script_obj = AV::Jobs::Script.new.from_file(filename, AV::Jobs::DSL::Commands)
+          @@script_obj = @@script_objs.pop if @@script_objs.size > 0
+          @@import = @@imports.pop if @@imports.size > 0
           @@prefixes.pop
 
-          script_obj
+          @@script_obj
         end
 
         def self.valid_jobs
@@ -25,6 +29,10 @@ module AV
 
         def self.filename
           @@import[:filename]
+        end
+
+        def self.script
+          @@script_obj
         end
 
         def self.prefix
