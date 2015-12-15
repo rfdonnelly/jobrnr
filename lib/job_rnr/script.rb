56a166309cd2636effbdd739705d30be4707bab3
Rob Donnelly <rfdonnelly@gmail.com>
2015-12-14 22:28:17 -0800

    fix #2 - basename as classname is insufficient
    Use anonymous class instead of named class.

--- lib/job_rnr/script.rb
+++ lib/job_rnr/script.rb
@@ -2,13 +2,8 @@ module JobRnr
   # Wraps a Ruby script in a Ruby class so that multiple scripts can be
   # loaded without namespace conflicts
   class Script
-    # Converts a filename (w/o extension) to a Ruby class name
-    def classify(name)
-      name.split("_").map(&:capitalize).join
-    end
-
-    def from_string(classname, code_string, filename = nil, base_class = nil)
-      class_obj = Object.const_set(classname, Class.new(base_class))
+    def from_string(code_string, filename = nil, base_class = nil)
+      class_obj = Class.new(base_class)
       obj = class_obj.new
       obj.instance_eval(code_string, filename)
 
@@ -17,10 +12,9 @@ module JobRnr
 
     def from_file(filename, base_class = nil)
       basename = File.basename(filename, '.jr')
-      classname = classify(basename)
 
       code_string = IO.read(filename)
-      from_string(classname, code_string, filename, base_class)
+      from_string(code_string, filename, base_class)
     end
   end
 end
