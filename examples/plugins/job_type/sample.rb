rfdonnelly/jobrnr

commit d0d084337bccd6f80381e8387f4ea69d590bb5ca

add options

lib/jobrnr/application.rb
@@ -7,7 +7,8 @@ module JobRnr
     end
 
     def run
-      filename = argv[0]
+      options = JobRnr::Options.new.parse(@argv)
+      filename = @argv[0]
 
       user_script = JobRnr::DSL::Loader.evaluate(nil, nil, filename)
 
@@ -19,26 +20,18 @@ module JobRnr
           directory_option
         end
 
-      JobRnr::Log.debug JobRnr::Graph.to_dot
-
-      # load plugins
-      if ENV.key?('JOBRNR_PLUGIN_PATH')
-        paths = ENV['JOBRNR_PLUGIN_PATH'].split(/:/)
-        JobRnr::Log.debug "Loading plugins from:\n#{paths.map { |path| "  #{path}" }.join("\n")}"
-        JobRnr::Plugins.instance.load(paths)
+      if options.dot
+        JobRnr::Log.info JobRnr::Graph.to_dot
+        exit
       end
 
-      job_slots =
-        if ENV.key?('JOBRNR_MAX_JOBS')
-          Integer(ENV['JOBRNR_MAX_JOBS'])
-        else
-          1
-        end
+      # load plugins
+      JobRnr::Plugins.instance.load(paths) unless options.plugin_paths.empty?
 
       JobRnr::Job::Dispatch.new(
         output_directory: output_directory,
         graph: JobRnr::Graph,
-        num_slots: job_slots
+        num_slots: options.max_jobs
       ).run
     end
   end

lib/jobrnr/options.rb
@@ -1,0 +1,66 @@
+module JobRnr
+  require 'optparse'
+  require 'ostruct'
+
+  class Options
+    attr_reader :options
+
+    def initialize
+      @options = initialize_options
+
+      default_options(@options)
+    end
+
+    def parse(argv)
+      OptionParser.new do |op|
+        op.banner = 'Usage: jobrnr [<option(s)>] <file.jr>'
+
+        op.separator('GENERAL OPTIONS')
+        op.on('-j', '--max-jobs <jobs>', 'Maximum number of jobs to run simultaneously') do |arg|
+          options.max_jobs = Integer(arg)
+        end
+        op.separator('')
+
+        op.separator('DEBUG OPTIONS')
+        op.on('--dot', 'Display job graph in GraphViz DOT format and exit') do
+          options.dot = true
+        end
+        op.separator('')
+
+        op.separator('MISCELLANEOUS OPTIONS')
+        op.on('-h', 'Display short help (this message)') do
+          puts op
+          exit
+        end
+        op.on('--help', 'Display long help') do
+          man_path = File.expand_path(File.join(File.dirname(__FILE__), '../../man/jobrnr.5.ronn'))
+          exec "ronn --man #{man_path}"
+        end
+      end.parse!(argv)
+
+      options
+    end
+
+    def initialize_options
+      OpenStruct.new
+    end
+
+    def default_options(options)
+      options.max_jobs = 
+        if ENV.key?('JOBRNR_MAX_JOBS') 
+          Integer(ENV['JOBRNR_MAX_JOBS']) 
+        else
+          1
+        end
+
+      options.plugin_paths = 
+        if ENV.key?('JOBRNR_PLUGIN_PATH')
+          ENV['JOBRNR_PLUGIN_PATH'].split(/:/)
+        else
+          []
+        end
+
+      options.dot = false
+    end
+  end
+end

lib/jobrnr.rb
@@ -2,6 +2,7 @@ require_relative 'jobrnr/application'
 require_relative 'jobrnr/dsl/commands'
 require_relative 'jobrnr/dsl/job_builder'
 require_relative 'jobrnr/dsl/loader'
+require_relative 'jobrnr/options'
 require_relative 'jobrnr/script'
 require_relative 'jobrnr/graph'
 require_relative 'jobrnr/stats'

commit 251cc367222b0871a9fe488e145b1abf13bf3c78

initial overhaul of plugin API
* Plugins are now instances of classes instead of static classes
* There is no longer a concept of plugin types.  There is now only one "type".
* Plugins can now respond to all events.
* Plugins no longer have a handles? method.  Instead, plugins decide if they
  want to respond to each event.
* Events are sent to JobRnr::Plugins singleton which then dispatches the
  event to each plugin.
* Fix d0d084337bccd6f80381e8387f4ea69d590bb5ca which broke plugins.
* Add events to plugin API: post_definition, post_interval, and terminate in
  addition to the pre-existing pre_instance and post_instance

examples/plugins/job_type/sample.rb
@@ -1,26 +1,22 @@
 module JobRnr
-  module JobType
+  module Plugin
     class Sample
-      def self.pre_process(job_instance)
+      def pre_instance(instance)
         # modify the job before it is run
         # add an option
-        unless job_instance.command.match(/\s--results-directory\b/)
-          results_directory = File.basename(job_instance.log, '.log')
-          job_instance.command << " --results-directory #{results_directory}"
+        unless instance.command.match(/\s--results-directory\b/)
+          results_directory = File.basename(instance.log, '.log')
+          instance.command << " --results-directory #{results_directory}"
         end
       end
 
-      def self.post_process(job_instance)
+      def post_instance(instance)
         # do something special for when a specific option is found
-        if job_instance.command.match(/\s--coverage\b/)
-          if job_instance.success?
-            puts "        doing something special for regr#{job_instance.slot}"
+        if instance.command.match(/\s--coverage\b/)
+          if instance.success?
+            puts "        doing something special for regr#{instance.slot}"
           end
         end
-      end
-
-      def self.handles?(command)
-        command.match(/.*/)
       end
     end
   end

lib/jobrnr/application.rb
@@ -10,6 +10,14 @@ module JobRnr
       options = JobRnr::Options.new.parse(@argv)
       filename = @argv[0]
 
+      if options.dot
+        JobRnr::Log.info JobRnr::Graph.instance.to_dot
+        exit
+      end
+
+      # load plugins
+      JobRnr::Plugins.instance.load(options.plugin_paths)
+
       user_script = JobRnr::DSL::Loader.instance.evaluate(nil, nil, filename)
 
       directory_option = JobRnr::Util.expand_envars(user_script.options.directory)
@@ -19,14 +27,6 @@ module JobRnr
         else
           directory_option
         end
-
-      if options.dot
-        JobRnr::Log.info JobRnr::Graph.instance.to_dot
-        exit
-      end
-
-      # load plugins
-      JobRnr::Plugins.instance.load(options.plugin_paths) unless options.plugin_paths.empty?
 
       JobRnr::Job::Dispatch.new(
         output_directory: output_directory,

lib/jobrnr/dsl/commands.rb
@@ -20,6 +20,7 @@ module JobRnr
           predecessors: predecessors
         )
         job = Docile.dsl_eval(builder, &block).build
+        JobRnr::Plugins.instance.post_definition(job)
         JobRnr::Graph.instance.add_job(job)
       end
 

lib/jobrnr/job/dispatch.rb
@@ -54,10 +54,8 @@ module JobRnr
           completed_instances = completed_futures.map(&:value)
           completed_instances.each do |job_instance|
             message(job_instance)
-
             stats.collect(job_instance)
-
-            job_instance.post_process
+            JobRnr::Plugins.instance.post_instance(job_instance)
 
             # find new jobs to be queued
             if job_instance.success? && job_instance.job.state.finished?
@@ -87,7 +85,7 @@ module JobRnr
             )
             job_queue.shift if job_instance.job.state.scheduled?
 
-            job_instance.pre_process
+            JobRnr::Plugins.instance.pre_instance(job_instance)
             message(job_instance)
             stats.collect(job_instance)
             future = Concurrent::Future.execute { job_instance.execute }
@@ -103,9 +101,12 @@ module JobRnr
             futures.push(future)
           end
           JobRnr::Log.info stats.to_s
+          JobRnr::Plugins.instance.post_interval
 
           sleep TIME_SLICE_INTERVAL
         end
+
+        JobRnr::Plugins.instance.terminate
 
         stats.failed
       end

lib/jobrnr/job/instance.rb
@@ -7,7 +7,6 @@ module JobRnr
       attr_reader :iteration
       attr_reader :log
       attr_reader :state
-      attr_reader :types
 
       def initialize(job:, slot:, log:)
         @job = job
@@ -17,7 +16,6 @@ module JobRnr
         @iteration = job.state.num_scheduled
         @status = nil
         @state = :pending
-        @types = initialize_types
 
         @start_time = Time.new
         @end_time = Time.new
@@ -25,33 +23,16 @@ module JobRnr
         job.state.schedule
       end
 
-      def pre_process
-        types.each { |type| type.pre_process(self) } if types
-      end
-
-      def post_process
-        types.each { |type| type.post_process(self) } if types
-      end
-
       def execute
         @start_time = Time.now
-
         @state = :dispatched
-
         @status = system("#{@command} > #{log} 2>&1")
-
         @state = :finished
-
         @end_time = Time.now
 
         job.state.complete
 
         self
-      end
-
-      def initialize_types
-        JobRnr::Plugins.instance.job_types
-          .select { |job_type| job_type.handles?(command) }
       end
 
       def duration

lib/jobrnr/options.rb
@@ -34,16 +34,21 @@ module JobRnr
           exit
         end
         op.on('--help', 'Display long help') do
-          man_path = JobRnr::Util.relative_to_file('../../man/jobrnr.1.ronn', __FILE__)
-          exec "ronn --man #{man_path}"
+          exec "ronn --man #{man_path('jobrnr.1.ronn')}"
         end
         op.on('--help-format', 'Display job description file format help') do
-          man_path = JobRnr::Util.relative_to_file('../../man/jobrnr.5.ronn', __FILE__)
-          exec "ronn --man #{man_path}"
+          exec "ronn --man #{man_path('jobrnr.5.ronn')}"
+        end
+        op.on('--help-plugin', 'Display plugin API help and exit.') do
+          exec "ronn --man #{man_path('jobrnr-plugin.3.ronn')}"
         end
       end.parse!(argv)
 
       options
+    end
+
+    def man_path(man_file)
+      JobRnr::Util.relative_to_file(File.join('../../man', man_file), __FILE__)
     end
 
     def initialize_options

lib/jobrnr/plugins.rb
@@ -1,8 +1,27 @@
 module JobRnr
   require 'singleton'
 
+  module PluginMethodStubs
+    PLUGIN_METHODS = [
+      :post_definition,
+      :pre_instance,
+      :post_instance,
+      :post_interval,
+      :terminate
+    ]
+
+    PLUGIN_METHODS.each do |meth|
+      define_method(meth) {}
+    end
+  end
+
   class Plugins
     include Singleton
+    include JobRnr::PluginMethodStubs
+
+    def initialize
+      @plugins = []
+    end
 
     # Public: Load plugins from path(s).
     #
@@ -27,13 +46,15 @@ module JobRnr
           require file
         end
       end
+
+      @plugins = create_plugin_instances(classes_in_module(JobRnr::Plugin))
     end
 
-    # Public: Returns all JobType plugins
-    #
-    # Returns Array of JobType plugins
-    def job_types
-      @job_types ||= classes_in_module(JobRnr::JobType)
+    # Dispatches plugin method calls to all plugin instances.
+    PLUGIN_METHODS.each do |meth|
+      define_method(meth) do |*args|
+        @plugins.each { |plugin| plugin.send(meth, *args) }
+      end
     end
 
     # Internal: Returns all Classes defined in a given Module.
@@ -50,9 +71,24 @@ module JobRnr
         .select { |c| Class === mod.const_get(c) }
         .map { |c| mod.const_get(c) }
     end
+
+    # Internal: Creates instances of plugin classes.
+    #
+    # Include PluginMethodStubs in each plugin so that they only need to define
+    # the events they are interested in.
+    #
+    # Examples
+    #
+    #   plugins = create_plugin_instances(classes)
+    #
+    # Returns array of plugin class instances
+    def create_plugin_instances(classes)
+      classes.map { |c| c.new }
+        .each { |o| o.class.send(:include, JobRnr::PluginMethodStubs) }
+    end
   end
 end
 
 module JobRnr
-  module JobType; end
+  module Plugin; end
 end

man/jobrnr-plugin.3.ronn
@@ -1,0 +1,23 @@
+jobrnr-plugin(3) -- plugin API for JobRnr
+=========================================
+
+## SYNOPSIS
+
+The JobRnr plugin API allows additional functionality to be added
+without changing the core.
+
+## TEMPLATE
+
+```ruby
+module JobRnr
+  module Plugin
+    class <plugin-class-name>
+      def post_definition(job); end
+      def pre_instance(job); end
+      def post_instance(job); end
+      def post_interval; end
+      def terminate; end
+    end
+  end
+end
+```

man/jobrnr.1.ronn
@@ -47,6 +47,9 @@ Miscellaneous options:
   * `--help-format`:
     Display job description file format help and exit.
 
+  * `--help-plugin`:
+    Display plugin API help and exit.
+
 ## ENVIRONMENT
 
   * `JOBRNR_PLUGIN_PATH`: