17adb433e941e4fcb678404d1e5ad9ddb8e867df
Rob Donnelly <rfdonnelly@gmail.com>
2016-02-15 18:13:08 -0800

    fix doc (mostly formatting)

--- lib/jobrnr/util.rb
+++ lib/jobrnr/util.rb
@@ -1,18 +1,19 @@
 module Jobrnr
   module Util
-    # expand environment variables in string
-    # supports '${name}' and '$name' syntax
-    # supports multiple environment variables in single string
+    # Expand environment variables in string.
+    #
+    # * Supports '${name}' and '$name' syntax
+    # * Supports multiple environment variables in single string
     def self.expand_envars(path)
       path.gsub(/\$\{?(\w+)\}?/) { ENV[Regexp.last_match.captures.first] }
     end
 
-    # makes from_file relative to to_file location
+    # Makes from_file relative to to_file location.
     #
     # Example:
     #
     #     relative_to_file('path_a/file_a', 'path_b/file_b')
-    #     => 'path_b/path_a/file_a'
+    #     => '$PWD/path_b/path_a/file_a'
     def self.relative_to_file(from_file, to_file)
       File.expand_path(File.join(File.dirname(to_file), from_file))
     end
