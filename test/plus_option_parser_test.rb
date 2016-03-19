9189c0a37cdcdbad7c373f1c705dac23d7ff62c6
Rob Donnelly <rfdonnelly@gmail.com>
2016-03-18 21:05:50 -0700

    convert plus option tests to ruby tests

--- test/plus_option_parser_test.rb
+++ test/plus_option_parser_test.rb
@@ -37,4 +37,133 @@ describe Jobrnr::PlusOptionParser do
       assert_equal(false, definition.default)
     end
   end
+
+  describe 'parse' do
+    before do
+      @specs = {
+        default_true: {
+          default: true,
+          doc: 'An option with a default true value.',
+        },
+        default_inferred: {
+          doc: 'An option with an inferred default value.',
+        },
+        fix_num: {
+          default: 1,
+          doc: 'A fixnum option.',
+        },
+        string: {
+          default: 'hello world',
+          doc: 'A string option.',
+        },
+      }
+    end
+
+    describe 'success' do
+      it 'supports defaults' do
+        exp = {
+          default_true: true,
+          default_inferred: false,
+          fix_num: 1,
+          string: 'hello world',
+        }
+        assert_equal(exp, @obj.parse(@specs, []))
+      end
+
+      it 'overrides defaults' do
+        exp = {
+          default_true: false,
+          default_inferred: true,
+          fix_num: 3,
+          string: 'hi',
+        }
+        assert_equal(exp, @obj.parse(@specs, %w(+default-inferred +default-true=false +fix-num=3 +string=hi)))
+      end
+
+      it 'overrides previous' do
+        exp = {
+          default_true: true,
+          default_inferred: false,
+          fix_num: 5,
+          string: 'hello',
+        }
+        act = @obj.parse(@specs, %w(
+                         +default-inferred
+                         +default-true=false
+                         +fix-num=3
+                         +string=hi
+                         +default-inferred=false
+                         +default-true
+                         +fix-num=5
+                         +string=hello
+                         ))
+        assert_equal(exp, act)
+      end
+
+      it 'accepts blank strings' do
+        exp = {
+          default_true: true,
+          default_inferred: false,
+          fix_num: 1,
+          string: '',
+        }
+        act = @obj.parse(@specs, %w(
+                         +string=
+                         ))
+        assert_equal(exp, act)
+      end
+    end
+
+    describe 'errors' do
+      it 'errors on unrecognized option' do
+        e = assert_raises(Jobrnr::ArgumentError) { @obj.parse(@specs, %w(+does-not-exist)) }
+
+        assert_equal(Jobrnr::Util.strip_heredoc(<<-EOF).strip, e.message)
+          The following options are not valid options: +does-not-exist
+
+          OPTIONS
+
+            +default-true[=<value>]
+              An option with a default true value. Default: true
+
+            +default-inferred[=<value>]
+              An option with an inferred default value. Default: false
+
+            +fix-num=<value>
+              A fixnum option. Default: 1
+
+            +string=<value>
+              A string option. Default: hello world
+        EOF
+      end
+
+      it 'errors on missing Integer argument' do
+        e = assert_raises(Jobrnr::ArgumentError) { @obj.parse(@specs, %w(+fix-num= +default-inferred)) }
+        assert_equal(Jobrnr::Util.strip_heredoc(<<-EOF).strip, e.message)
+          Could not parse '' as Integer type for the '+fix-num' option
+        EOF
+      end
+
+      it 'errors on bad argument syntax' do
+        e = assert_raises(Jobrnr::ArgumentError) { @obj.parse(@specs, %w(+fix-num 2)) }
+        assert_equal(Jobrnr::Util.strip_heredoc(<<-EOF).strip, e.message)
+          No argument given for '+fix-num' option
+        EOF
+      end
+
+      it 'errors on bad Integer format' do
+        e = assert_raises(Jobrnr::ArgumentError) { @obj.parse(@specs, %w(+fix-num=five)) }
+        assert_equal(Jobrnr::Util.strip_heredoc(<<-EOF).strip, e.message)
+          Could not parse 'five' as Integer type for the '+fix-num' option
+        EOF
+      end
+
+      it 'errors on bad Boolean format' do
+        e = assert_raises(Jobrnr::ArgumentError) { @obj.parse(@specs, %w(+default-true=five)) }
+        assert_equal(Jobrnr::Util.strip_heredoc(<<-EOF).strip, e.message)
+          Could not parse 'five' as Boolean type for the '+default-true' option
+        EOF
+      end
+    end
+  end
 end
