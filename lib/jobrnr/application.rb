rfdonnelly/jobrnr

commit 4d95bf26cf259e52a15f224ee8eba67a56f48afc

Validate arguments
Ensure we are given exactly 1 filename and the file exists.

Fixes #34

lib/jobrnr/application.rb
@@ -9,7 +9,7 @@ module Jobrnr
     def run
       begin
         run_with_exceptions
-      rescue OptionParser::ParseError => e
+      rescue OptionParser::ParseError, Jobrnr::UsageError => e
         Jobrnr::Log.error [e.message, 'See `jobrnr --help`'].join("\n\n")
       rescue Jobrnr::HelpException => e
         puts e.message
@@ -21,9 +21,13 @@ module Jobrnr
 
     def run_with_exceptions
       options = Jobrnr::Options.new.parse(@argv)
-      filename = @argv.shift
-      plus_options = @argv
+      filenames, plus_options = classify_arguments(@argv)
 
+      raise Jobrnr::UsageError, "missing filename argument" if filenames.nil?
+      raise Jobrnr::UsageError, "unrecognized option(s): #{filenames[1..-1].join(' ')}" if filenames.size > 1
+
+      filename = filenames.first
+      raise Jobrnr::Error, "file does not exist: #{filename}" unless File.exists?(filename)
 
       # load plugins
       Jobrnr::Plugins.instance.load(options.plugin_paths)
@@ -41,6 +45,16 @@ module Jobrnr
         graph: Jobrnr::Graph.instance,
         num_slots: options.max_jobs
       ).run
+    end
+
+    def classify_arguments(argv)
+      argv.group_by do |arg|
+        if arg[0] == '+'
+          :plus_option
+        else
+          :filename
+        end
+      end.values
     end
 
     def merge_options(global_options, user_script_options, user_script_filename)

lib/jobrnr/error.rb
@@ -2,6 +2,7 @@ module Jobrnr
   # All errors raised by Jobrnr code shall be Jobrnr::Error or derived
   # from Jobrnr::Error
   class Error < StandardError; end
+  class UsageError < Error; end
   class OptionsError < Error; end
   class ImportError < Error; end
   class ArgumentError < Error; end

commit d00070300a133bd38bc6f575f7341e4b65b4ef6c

Fix plus_options nil when no plus options
* Caused by 4d95bf2
* Caught by regression
* Fixes regression

lib/jobrnr/application.rb
@@ -48,13 +48,15 @@ module Jobrnr
     end
 
     def classify_arguments(argv)
-      argv.group_by do |arg|
+      hash = argv.group_by do |arg|
         if arg[0] == '+'
-          :plus_option
+          :plus_options
         else
-          :filename
+          :filenames
         end
-      end.values
+      end
+
+      [:filenames, :plus_options].map { |key| Array(hash[key]) }
     end
 
     def merge_options(global_options, user_script_options, user_script_filename)