rfdonnelly/jobrnr

commit f2e739b091a641d7be1fbc2eab7ffec200311e9e

Add --verbose option to enable debug output

lib/jobrnr/log.rb
@@ -1,6 +1,18 @@
 module Jobrnr
   class Log
+    VERBOSITY_SEVERITY_MAP = {
+      ERROR: -999,
+      INFO: 1,
+      DEBUG: 2,
+    }
+
+    def self.verbosity=(verbosity)
+      @@verbosity = verbosity
+    end
+
     def self.report(severity, message)
+      return unless @@verbosity >= VERBOSITY_SEVERITY_MAP[severity]
+
       out = (severity == :ERROR) ? STDERR : STDOUT
       prefix = (severity == :INFO) ? '' : "#{severity}: jobrnr: "
       out.puts [prefix, message].join

lib/jobrnr/application.rb
@@ -21,6 +21,7 @@ module Jobrnr
 
     def run_with_exceptions
       options = Jobrnr::Options.new.parse(@argv)
+      Log.verbosity = options.verbosity
       filenames, plus_options = classify_arguments(@argv)
 
       raise Jobrnr::UsageError, "missing filename argument" if filenames.nil?

lib/jobrnr/options.rb
@@ -18,6 +18,9 @@ module Jobrnr
         op.banner = 'Usage: jobrnr [<option(s)>] <file.jr>'
 
         op.separator('GENERAL OPTIONS')
+        op.on('-v', '--vebose', 'Enable debug output.') do |arg|
+          options.verbosity += 1
+        end
         op.on('-d', '--output-directory <directory>',
               'Directory to place results.',
               String) do |arg|
@@ -72,7 +75,8 @@ module Jobrnr
         :max_failures,
         :max_jobs,
         :output_directory,
-        :plugin_paths
+        :plugin_paths,
+        :verbosity,
       ).new
     end
 
@@ -82,6 +86,7 @@ module Jobrnr
       options.max_jobs = 8
       options.output_directory = Dir.pwd
       options.plugin_paths = []
+      options.verbosity = 1
     end
 
     def load_environment(options)

man/jobrnr.1.ronn
@@ -24,6 +24,9 @@ files are typically named <name>.jr (e.g. `example.jr`).
 
 General options:
 
+  * `-v`, `--verbose`:
+    Enable debug output.
+
   * `-d`, `--output-directory` <directory>:
     The directory where results will be placed.  Will be created if it does
     not exist.  Overrides the JOBRNR_OUTPUT_DIRECTORY environment variable.

commit 4c3dc3c7ca00e9eb82d46a8c5a6e1066b3d41ce0

Fix stacktrace on bad option
f2e739b introduced a regression that caused a stacktrace on errors
happening before Log::@@verbosity was initailized.

Fix by initailizing @@verbosity.

Fixes #39

lib/jobrnr/log.rb
@@ -6,6 +6,8 @@ module Jobrnr
       DEBUG: 2,
     }
 
+    @@verbosity = 0
+
     def self.verbosity=(verbosity)
       @@verbosity = verbosity
     end