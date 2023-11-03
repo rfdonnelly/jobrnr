65bfaffcc98e216a7926cdee8d57b68e5cf74010
Rob Donnelly <rfdonnelly@gmail.com>
2023-11-02 23:39:03 -0700

    test: cleanup directories created by jobrnr

--- test/cli_job_exit_status_test.rb
+++ test/cli_job_exit_status_test.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "securerandom"
 
 describe "CLI Job Exit Status" do
   def no_color(&block)
     ENV["NO_COLOR"] = "1"
     block.call
     ENV.delete("NO_COLOR")
+  end
+
+  def tmpdir(&block)
+    path = format("test-%s", SecureRandom.urlsafe_base64)
+    block.call(path)
+    FileUtils.rm_rf(path) if Dir.exist?(path)
   end
 
   def assert_io_matches(exp_out, exp_err, &block)
@@ -25,31 +32,37 @@ describe "CLI Job Exit Status" do
   it "fails" do
     exp_out = /FAILED: 'false'/
     assert_io_matches(exp_out, "") do
-      Jobrnr::Application.new(%w[test/fixtures/job_exit_status/fail.rb -d fail]).run
+      tmpdir do |path|
+        Jobrnr::Application.new(["test/fixtures/job_exit_status/fail.rb", "-d", path]).run
+      end
     end
   end
 
   it "passes" do
     exp_out = /PASSED: 'true'/
     assert_io_matches(exp_out, "") do
-      Jobrnr::Application.new(%w[test/fixtures/job_exit_status/pass.rb -d pass]).run
+      tmpdir do |path|
+        Jobrnr::Application.new(["test/fixtures/job_exit_status/pass.rb", "-d", path]).run
+      end
     end
   end
 
   it "passes and fails" do
-    out, err = capture_subprocess_io do
-      Jobrnr::Application.new(%w[test/fixtures/job_exit_status/pass_and_fail.rb -j1 -d pass_and_fail]).run
+    tmpdir do |path|
+      out, err = capture_subprocess_io do
+        Jobrnr::Application.new(["test/fixtures/job_exit_status/pass_and_fail.rb", "-j1", "-d", path]).run
+      end
+
+      expect(out).must_match(/PASSED: 'job 0' slot:recycled exitcode:0/)
+      expect(out).must_match(/FAILED: 'job 1' slot:0 exitcode:1/)
+      expect(out).must_match(/FAILED: 'job 42' slot:1 exitcode:42/)
+      expect(out).must_match(%r{FAILED: 'command_not_found arg' slot:2 exitcode:n/a})
+      expect(err).must_equal ""
+
+      command_not_found_output = File.read(File.join(path, "2"))
+      expect(command_not_found_output).must_equal("ERROR: failed to spawn command 'command_not_found arg' " \
+                                                  "for job 'command_not_found': No such file or directory " \
+                                                  "- command_not_found")
     end
-
-    expect(out).must_match(/PASSED: 'job 0' slot:recycled exitcode:0/)
-    expect(out).must_match(/FAILED: 'job 1' slot:0 exitcode:1/)
-    expect(out).must_match(/FAILED: 'job 42' slot:1 exitcode:42/)
-    expect(out).must_match(%r{FAILED: 'command_not_found arg' slot:2 exitcode:n/a})
-    expect(err).must_equal ""
-
-    command_not_found_output = File.read("pass_and_fail/2")
-    expect(command_not_found_output).must_equal("ERROR: failed to spawn command 'command_not_found arg' " \
-                                                "for job 'command_not_found': No such file or directory " \
-                                                "- command_not_found")
   end
 end
